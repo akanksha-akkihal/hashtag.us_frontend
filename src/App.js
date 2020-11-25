8cbce70b0e0c4d58e2a5464041635b41b121a1fb
Akanksha27 <akankshaakkihal27@gmail.com>
2020-11-25 20:12:52 +0530

    integrated with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/HomePage/Home';
+import {Navbar} from './components';
+import Featurepage from './pages/HomePage/Featurepage';
+import Aboutpage from './pages/HomePage/Aboutpage';
+import GetStartedPage from './pages/HomePage/GetStartedPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <GlobalStyle/>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component ={Home} />
+        <Route path="/features" exact component ={Featurepage}/>
+        <Route path="/about-us" exact component={Aboutpage}/>
+        <Route path="/sign-up" exact component={GetStartedPage}/>
+      </Switch>
+      
+    </Router>
+      
+      
     </div>
   );
 }
